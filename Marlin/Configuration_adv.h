638019ddafe5a046589db55199bc758db1033d18
Timmiej93 <16491748+Timmiej93@users.noreply.github.com>
2019-12-13 12:09:10 +0100

    Increased Hybrid Thresholds
    Z-axis was making a lot of noise, due to hybrid threshold being too low.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2092,11 +2092,11 @@
    */
   #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     100  // [mm/s]
+  #define X_HYBRID_THRESHOLD     120  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     100
+  #define Y_HYBRID_THRESHOLD     120
   #define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD       3
+  #define Z_HYBRID_THRESHOLD      15
   #define Z2_HYBRID_THRESHOLD      3
   #define Z3_HYBRID_THRESHOLD      3
   #define E0_HYBRID_THRESHOLD     30
